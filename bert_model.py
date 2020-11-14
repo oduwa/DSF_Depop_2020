486d89317779fa53c90d47e41b08a872d00e9694
oduwa <garyedoosagie@yahoo.com>
2020-11-14 11:06:44 +0000

    update for bert model after workshop

--- bert_model.py
+++ bert_model.py
@@ -14,55 +14,69 @@ import pandas as pd
 
 
 class BERT(object):
-    def __init__(self, bert_path, max_seq_length=64, bert_tune_layers=10):
+    def __init__(self, bert_path, max_seq_length=64, bert_tune_layers=3):
         self.max_seq_length = max_seq_length
         self.bert_tune_layers = bert_tune_layers
-        self.bert_path = bert_path
         self.session = tf.Session()
-        self.featurizer = BertFeaturizer(self.bert_path, self.session, max_seq_length=max_seq_length)
+        self.featurizer = BertFeaturizer(bert_path,
+                                         self.session,
+                                         max_seq_length=self.max_seq_length)
         self.model = self.__build_model()
         self.__initialize_vars(self.session)
 
-    def __initialize_vars(self, session):
-        session.run(tf.local_variables_initializer())
-        session.run(tf.global_variables_initializer())
-        session.run(tf.tables_initializer())
-        K.set_session(session)
-
     def __build_model(self):
+        # Initialize inputs
         in_id = tf.keras.layers.Input(shape=(self.max_seq_length,), name="input_ids")
         in_mask = tf.keras.layers.Input(shape=(self.max_seq_length,), name="input_masks")
         in_segment = tf.keras.layers.Input(shape=(self.max_seq_length,), name="segment_ids")
         bert_inputs = [in_id, in_mask, in_segment]
 
+        # Setup architecture
         bert_output = BertLayer(n_fine_tune_layers=self.bert_tune_layers)(bert_inputs)
         dense = tf.keras.layers.Dense(256, activation="relu")(bert_output)
         pred = tf.keras.layers.Dense(1, activation="sigmoid")(dense)
 
+        # Put it together
         model = tf.keras.models.Model(inputs=bert_inputs, outputs=pred)
         model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
         model.summary()
 
         return model
 
+    def __initialize_vars(self, sess):
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+        K.set_session(sess)
+
     def fit(self, texts, labels, validation_ratio=0.1):
+        # Divide data into training and validation splits
         val_texts = texts[:int(validation_ratio*len(texts))]
-        train_texts = texts[int(validation_ratio * len(texts)):]
-        val_labels = labels[:int(validation_ratio * len(texts))]
-        train_labels = labels[int(validation_ratio * len(texts)):]
+        train_texts = texts[int(validation_ratio*len(texts)):]
+        val_labels = labels[:int(validation_ratio*len(labels))]
+        train_labels = labels[int(validation_ratio*len(labels)):]
 
-        train_input_ids, train_input_masks, train_segment_ids, train_labels = self.featurizer.convert_text_to_features(train_texts,
-                                                                                                         train_labels)
-        val_input_ids, val_input_masks, val_segment_ids, val_labels = self.featurizer.convert_text_to_features(val_texts,
-                                                                                                   val_labels)
+        # Prepare data for BERT input
+        train_input_ids, train_input_masks, train_segment_ids, train_labels = \
+            self.featurizer.convert_text_to_features(train_texts, train_labels)
+        val_input_ids, val_input_masks, val_segment_ids, val_labels = \
+            self.featurizer.convert_text_to_features(val_texts, val_labels)
 
-        self.model.fit([train_input_ids, train_input_masks, train_segment_ids],
-                       train_labels,
-                       validation_data=([val_input_ids, val_input_masks, val_segment_ids], val_labels),
-                       epochs=2,
-                       batch_size=32)
+        # train
+        self.model.fit(
+            [train_input_ids, train_input_masks, train_segment_ids],
+            train_labels,
+            validation_data=(
+                [val_input_ids, val_input_masks, val_segment_ids],
+                val_labels,
+            ),
+            epochs=2,
+            batch_size=32,
+        )
 
-
+    def predict_proba(self, X):
+        input_ids, input_masks, segment_ids, labels = self.featurizer.convert_text_to_features(X, None)
+        return self.model.predict([input_ids, input_masks, segment_ids, labels])
 
 
 if __name__ == "__main__":
@@ -74,6 +88,8 @@ if __name__ == "__main__":
     texts, labels = dataset["text"].tolist(), dataset["label"].tolist()
 
     my_bert.fit(texts, labels)
+
+    print(my_bert.predict_proba(["free free sexy £1"]))
 
 
 
